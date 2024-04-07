cc9ce6f3ce2176b7d4035f7e7698a1ab39d2cc54
godcards <2929362461@qq.com>
2024-04-07 09:11:00 +0800

    空编辑页面

---
+++ src/components/module/cource_page/blank_edit.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  data() {
+    return {
+      step: 0
+    };
+  },
+  methods: {
+    gotu() {
+      this.step = 1
+
+    },
+    back() {
+      this.step = 0
+    }
+  }
+
+}
+</script>
+<template>
+  <div id="main_cource">
+    <v-icon>mdi-arrow-left</v-icon>
+    <div id="edit_cource">
+      <v-stepper v-model="step">
+        <v-stepper-header>
+          <v-stepper-item icon="mdi-information-outline" title="课程基本信息" value="1" step="1">
+          </v-stepper-item>
+          <v-divider></v-divider>
+          <v-stepper-item icon=" mdi-format-list-bulleted" title="课程内容" value="2" step="2">
+          </v-stepper-item>
+        </v-stepper-header>
+        <v-stepper-window>
+          <v-stepper-window-item v-for="n in [1, 2]" :key="`${n}-content`">
+          </v-stepper-window-item>
+          <v-stepper-window-item v-for="n in [1, 2]" :key="`${n}-content`">
+          </v-stepper-window-item>
+        </v-stepper-window>
+      </v-stepper>
+    </div>
+  </div>
+</template>
+<style>
+#main_cource {
+  display: flex;
+  margin: 22px 220px 108px 148px;
+}
+
+#edit_cource {
+  height: 100%;
+  width: 100%;
+  margin-left: 40px;
+  background: white;
+}
+</style>
