e9200a4adec50c64c993469dd33877eee8f912d3
CHEN Prime <chenbprime@outlook.com>
2024-05-11 14:59:02 +0800

    重构：重构数据分析页面

--- src/views/teacher/statistics/DataAnalysisView.vue
+++ src/views/teacher/statistics/DataAnalysisView.vue
@@ -15,70 +15,48 @@
         class="text-white text-center text-3xl block w-full relative"
       >
         智慧校园数字可视化系统
-        <svg
-          id="data-analysis-title-home-button"
-          width="34"
-          height="34"
-          viewBox="0 0 34 34"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          @click="$router.push('/')"
+        <div
+          id="data-analysis-title-end"
+          class="flex justify-center items-center"
         >
-          <path
-            opacity="0.1"
-            d="M30.8301 32.97H3.15002L1 30.8199V24.5399L2.20007 23.4099L2.22009 10.58L1 9.34998V3.14001L3.15002 1H30.8301L32.9701 3.14001V30.8199L30.8301 32.97Z"
-            fill="#009DFF"
-          />
-          <path
-            opacity="0.6"
-            d="M30.8301 32.97H3.15002L1 30.8199V24.5399L2.20007 23.4099L2.22009 10.58L1 9.34998V3.14001L3.15002 1H30.8301L32.9701 3.14001V30.8199L30.8301 32.97Z"
-            stroke="#009DFF"
-            stroke-width="0.58"
-            stroke-miterlimit="10"
-          />
-          <path
-            d="M15 25V19H19V25H24V17H27L17 8L7 17H10V25H15Z"
-            fill="#009DFF"
-          />
-        </svg>
-
+          <svg
+            id="data-analysis-title-home-button"
+            width="34"
+            height="34"
+            viewBox="0 0 34 34"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            @click="$router.push('/')"
+          >
+            <path
+              opacity="0.1"
+              d="M30.8301 32.97H3.15002L1 30.8199V24.5399L2.20007 23.4099L2.22009 10.58L1 9.34998V3.14001L3.15002 1H30.8301L32.9701 3.14001V30.8199L30.8301 32.97Z"
+              fill="#009DFF"
+            />
+            <path
+              opacity="0.6"
+              d="M30.8301 32.97H3.15002L1 30.8199V24.5399L2.20007 23.4099L2.22009 10.58L1 9.34998V3.14001L3.15002 1H30.8301L32.9701 3.14001V30.8199L30.8301 32.97Z"
+              stroke="#009DFF"
+              stroke-width="0.58"
+              stroke-miterlimit="10"
+            />
+            <path
+              d="M15 25V19H19V25H24V17H27L17 8L7 17H10V25H15Z"
+              fill="#009DFF"
+            />
+          </svg>
+          <span class="absolute right-[6rem]">
+            2024/05/11 13:48:24
+          </span>
+        </div>
       </span>
       <div
         id="data-analysis-content"
-        class="w-full flex-grow p-2 grid gap-2"
+        class="w-full h-full flex-grow p-2 grid gap-2"
       >
-        <dv-border-box1 class="w-full h-full chart-box">
-          <dv-decoration7 class="h-[1rem]">
-            <div class="text-white mx-2">
-              授课数量统计
-            </div>
-          </dv-decoration7>
-          <the-course-count-bar
-            class="chart-item"
-            @click="handleCourseCountBarChartClick"
-          />
-        </dv-border-box1>
-        <dv-border-box1 class="w-full h-full chart-box">
-          <dv-decoration7 class="h-[1rem]">
-            <div class="text-white mx-2">
-              授课数量统计
-            </div>
-          </dv-decoration7>
-        </dv-border-box1>
-        <dv-border-box1 class="w-full h-full chart-box">
-          <dv-decoration7 class="h-[1rem]">
-            <div class="text-white mx-2">
-              授课数量统计
-            </div>
-          </dv-decoration7>
-        </dv-border-box1>
-        <dv-border-box1 class="w-full h-full chart-box">
-          <dv-decoration7 class="h-[1rem]">
-            <div class="text-white mx-2">
-              授课数量统计
-            </div>
-          </dv-decoration7>
-        </dv-border-box1>
+        <div class="flex h-full w-full flex-col gap-2">
+          <the-course-count-bar class="h-full w-full" />
+        </div>
       </div>
     </div>
   </div>
@@ -86,16 +64,12 @@
 
 <script>
 import mitt from '@/plugins/mitt'; 
-import { BorderBox1 as DvBorderBox1 } from '@kjgl77/datav-vue3';
 import { Loading as DvLoading } from '@kjgl77/datav-vue3';
-import { Decoration7 as DvDecoration7 } from '@kjgl77/datav-vue3';
 
 export default {
   name: 'DataAnalysisView',
   components: {
-    DvBorderBox1: DvBorderBox1,
     DvLoading: DvLoading,
-    DvDecoration7: DvDecoration7,
   },
   data: () => ({
     loading: false
@@ -115,31 +89,35 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #data-analysis {
-  background-image: url('@/assets/img/conny-schneider-xuTJZ7uD7PI-unsplash.jpg');
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
+  background-color: #000d4a;
+  font-family: SimSun;
 }
 
 #data-analysis-title {
-  background-image: url('@/assets/img/analysis/title-bg.svg');
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  padding: 1rem;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    font-size: 1rem;
+    display: block;
+    color: #d6d6d6;
+  }
 }
 
 #data-analysis-content {
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1.5fr 1fr;
 }
 
 #data-analysis-title-home-button {
   position: absolute;
-  top: 1rem;
-  right: 1rem;
+  right: 2rem;
   width: 2.5rem;
   height: 2.5rem;
   z-index: 9999;
@@ -160,14 +138,28 @@ export default {
 }
 
 .chart-item {
-  width: calc(100% - 3rem);
-  height: calc(100% - 2rem);
-  margin-bottom: 1rem;
+  background-color: #031e5c;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 }
 
-.chart-box {
-  background-color: #010101c4;
-  box-shadow: 0 0 10px 0 #010101c4;
+.chart-title {
+  color: white;
+  font-weight: bold;
+  margin-left: 1.05rem;
+  width: 100%;
+  margin-top: 0.5rem;
+}
+
+.chart-title::before {
+  content: '';
+  display: inline-block;
+  width: 0.25rem;
   border-radius: 1rem;
+  height: 1.05rem;
+  background-color: #49bcf7;
+  vertical-align: text-bottom;
 }
 </style>
