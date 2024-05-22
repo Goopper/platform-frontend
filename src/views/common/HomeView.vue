09638287e8eb3a497f4ec9bd6341b4aecc2d590b
CHEN Prime <chenbprime@outlook.com>
2024-05-22 13:51:43 +0800

    修复：修复首页重构之后的一些响应式问题

--- src/views/common/HomeView.vue
+++ src/views/common/HomeView.vue
@@ -3,9 +3,9 @@
     <!-- start -->
     <router-view class="blg:flex-grow lg:mr-0 sm:mb-2 bmd:mr-8" />
     <!-- end -->
-    <div class="flex gap-8 flex-col lg:min-h-[640px] h-full w-1/3 min-w-96 sm:w-full md:w-full md:mt-8 sm:min-w-0 sm:mb-2 lg:w-full lg:grow">
-      <div class="h-auto">
-        <the-plugin-list-card class="sm:hidden" />
+    <div class="flex gap-8 flex-col h-full w-1/3 min-w-96 sm:w-full md:w-full sm:mt-0 bsm:mt-8 bmd:mt-0 sm:min-w-0 sm:mb-2 lg:w-full lg:grow">
+      <div class="h-auto sm:hidden">
+        <the-plugin-list-card />
       </div>
       <the-message-list-card />
     </div>
