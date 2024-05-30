55b55d1a1db506f550ce826fb7e9c4119298133f
godcards <2929362461@qq.com>
2024-05-30 18:25:34 +0800

    添加 : 添加了查看已批改的作业

--- src/views/teacher/TaskCorrectView.vue
+++ src/views/teacher/TaskCorrectView.vue
@@ -2,269 +2,150 @@
   <div>
     <custom-float-back-button />
     <div class="flex flex-col border bg-secondary h-full">
-      <div class="border-b p-4 flex justify-between items-center">
-        <p class="text-lg">
-          <span>{{ answer.studentName }} ({{ answer.groupName }})</span>
-        </p>
-        <div
-          v-if="answer.nextAnswerUserMessageId"
-          class="flex items-center justify-center"
-        >
-          <div class="text-end text-gray-500">
-            <p>
-              下一份：{{ answer.nextAnswerTaskName }}
-            </p>
-            <p>
-              {{ answer.nextAnswerStudentName }} ({{ answer.nextAnswerGroupName }})
-            </p>
-          </div>
-          <v-btn
-            id="next-answer-btn"
-            class="ml-4"
-            density="comfortable"
-            variant="outlined"
-            icon="mdi-arrow-right"
-            @click="handleNextButtonClick"
-          />
-          <v-tooltip
-            activator="#next-answer-btn"
-            location="top"
-          >
-            点击前往下一份需要批改的作业
-          </v-tooltip>
-        </div>
-      </div>
-      <!-- answer content area -->
-      <div class="border-b p-4 overflow-y-auto">
-        <p class="font-bold">
-          {{ answer.taskName }}
-        </p>
+      <div class="StudentInfo">
+        <h1>{{ answer.studentName }}({{ answer.number }}) - {{ answer.groupName }}</h1>
         <p>
-          {{ answer.content }}
+          分数 : <span :style="{ color: getScoreColor(correct.score) }">{{ correct.score }}</span>
         </p>
       </div>
-      <!-- attachments area -->
-      <div class="flex-grow border-b p-4">
-        <div
-          v-if="answer.attachments && answer.attachments.length===0"
-          class="flex flex-col justify-center items-center"
-        >
-          <v-icon
-            size="50px"
-            class="text-gray-400"
-            icon="mdi-message-off-outline"
-          />
-          <p class="font-bold text-lg text-gray-400">
-            无附件
-          </p>
-        </div>
-        <div
-          v-else
-          class="max-h-[150px] overflow-y-auto flex flex-col gap-4"
-        >
+      <div class="taskInfo">
+        <h1>题目名称 : {{ answer.taskName }}</h1>
+        <p>{{ answer.courseName }}-{{ answer.sectionName }}</p>
+      </div>
+      <div class="content">
+        <p>题目内容 :</p>
+        <span>{{ correct.taskContent }}</span>
+      </div>
+      <div class="answer-content">
+        <p>答案 : </p>
+        <span>{{ correct.answerContent }}</span>
+      </div>
+      <div class="time">
+        提交时间 : {{ formatTime(answer.submitTime) }}
+      </div>
+      <div class="attachments">
+        附件 :
+        <div v-if="correct.attachments.length > 0">
           <custom-attachment-card
-            v-for="attachment in answer.attachments"
+            v-for="attachment in correct.attachments"
             :key="attachment.id"
+            class="attachment-card"
             :attachment="attachment"
           />
         </div>
+        <div v-else>
+          <p>当前作业无附件</p>
+        </div>
       </div>
-      <!-- comment area -->
-      <div class="border-b p-4">
-        <v-textarea
-          v-model="comment"
-          no-resize
-          rows="2"
-          density="compact"
-          hide-details
-          clearable
-          :maxlength="200"
-          variant="outlined"
-          label="评语（最多200字）"
-        />
-      </div>
-      <!-- actions area -->
-      <div class="flex p-4 bg-background-important">
-        <div class="flex-grow" />
-        <v-text-field
-          v-model="score"
-          class="max-w-[200px]"
-          color="background-secondary-actions"
-          variant="outlined"
-          density="compact"
-          type="number"
-          min="0"
-          max="100"
-          hide-details
-          bg-color="white"
-          label="分数"
-        >
-          <template #append>
-            <v-btn
-              variant="flat"
-              :disabled="disable"
-              @click="handleSubmitCorrect"
-            >
-              提交
-            </v-btn>
-          </template>
-        </v-text-field>
+      <div class="comment">
+        <div v-if="correct.comment">
+          {{ correct.comment }}
+        </div>
+        <div v-else>
+          <p>无评语</p>
+        </div>
       </div>
     </div>
-    <!-- dialog -->
-    <v-dialog
-      v-model="confirmDialog"
-      persistent
-      max-width="500"
-    >
-      <v-card
-        title="提交批改"
-        color="white"
-      >
-        <v-card-text>
-          确定要提交批改吗？提交之后无法修改。
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            variant="flat"
-            class="ms-auto"
-            text="确认"
-            :loading="loading"
-            @click="clickSubmitCorrect"
-          />
-          <v-btn
-            variant="outlined"
-            text="取消"
-            :disabled="loading"
-            @click="confirmDialog = false"
-          />
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-dialog
-      v-model="nextConfirmDialog"
-      persistent
-      max-width="500"
-    >
-      <v-card
-        title="提示"
-        color="white"
-      >
-        <v-card-text>
-          作业批改完毕，要前往下一份作业吗？
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            variant="flat"
-            class="ms-auto"
-            text="确认"
-            :loading="loading"
-            @click="handleNextButtonClick"
-          />
-          <v-btn
-            variant="outlined"
-            text="取消"
-            :disabled="loading"
-            @click="handleNextCancelButtonClick"
-          />
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </div>
 </template>
 
 <script>
-import { getAnswerByMessageId, submitCorrect } from '@/api/correct';
-import { receiveOneMessage } from '@/api/message';
-import mitt from '@/plugins/mitt';
-import { useSaveStore } from '@/store/common/save';
-
+import { CorrectedAnswer } from '@/api/correct';
 export default {
   name: 'TaskCorrectView',
   data: () => ({
-    messageId: null,
     loading: true,
     answer: {},
-    rawScore: null,
-    comment: '',
-    confirmDialog: false,
-    nextConfirmDialog: false,
-    saveStore: useSaveStore(),
+    correct : null,
   }),
-  computed: {
-    score: {
-      get() {
-        return this.rawScore;
-      },
-      set(value) {
-        if (value < 0) {
-          this.rawScore = 0;
-        } else if (value > 100) {
-          this.rawScore = 100;
-        } else {
-          this.rawScore = value;
-        }
-      },
-    },
-    disable() {
-      const isChanged = this.score !== null;
-      this.saveStore.setSaveState(!isChanged);
-      return !isChanged;
-    }
-  },
   created() {
-    this.messageId = this.$route.params.messageId;
+    this.correctId = this.$route.params.id;
     this.loadAnswer();
   },
   methods: {
     async loadAnswer() {
       this.loading = true;
-      const res = await getAnswerByMessageId(this.messageId);
+      const res = await CorrectedAnswer(this.correctId);
       if (res) {
-        this.answer = res.data;
+        this.correct = res.data;
+        this.answer = res.data.answer;
       }
       this.loading = false;
     },
-    handleNextButtonClick() {
-      const nextId = this.answer.nextAnswerUserMessageId;
-      this.messageId = nextId;
-      this.$router.replace({ path: `/teacher/correct/${nextId}` });
-      this.rawScore = 0;
-      this.comment = '';
-      this.loadAnswer();
-      this.nextConfirmDialog = false;
-    },
-    handleSubmitCorrect() {
-      this.confirmDialog = true;
-    },
-    async clickSubmitCorrect() {
-      this.loading = true;
-      const res = await submitCorrect(this.answer.answerId, this.comment, this.score);
-      await receiveOneMessage(this.messageId);
-      if (res) {
-        mitt.emit('showToast', {
-          title: '提交成功！',
-          color: 'success',
-          icon: '$success',
-        });
-        this.confirmDialog = false;
-        if (this.answer.nextAnswerUserMessageId) {
-          this.nextConfirmDialog = true;
-        } else {
-          this.$router.push('/personal/message');
-        }
+    getScoreColor(score) {
+      if (score >= 90) {
+        return 'green';
+      } else if (score >= 60) {
+        return 'orange';
       } else {
-        mitt.emit('toast', {
-          type: 'error',
-          message: '提交失败'
-        });
+        return 'red';
       }
-      this.loading = false;
     },
-    handleNextCancelButtonClick() {
-      this.nextConfirmDialog = false;
-      this.$router.go(-1);
+     formatTime(timeString) {
+      const date = new Date(timeString);
+      return date.toLocaleDateString('zh-CN', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' });
     },
   },
 };
 </script>
+<style lang="scss" scoped>
+.StudentInfo{
+  height: 9%;
+  display: flex;
+  justify-content: space-between;
+  padding: 1.25em;
+  border-bottom: 1px solid #e0e0e0;
+  h1{
+    font-size: 1.25em;
+    font-weight: bold;
+  }
+  p{
+    font-weight: bold;
+  }
+}
+.taskInfo{
+  height: 6%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.75em;
+  border-bottom: 1px solid #e0e0e0;
+  h1{
+    font-weight: bold;
+  }
+}
+.content,.answer-content{
+  height: 18%;
+  padding: 1em;
+  border-bottom: 1px solid #e0e0e0;
+  span{
+     margin: 0.5em;
+  }
+}
+.answer-content{
+  border-bottom: 0;
+  span{
+    margin: 0.5em;
+  }
+}
+.attachments{
+  height: 24%;
+  border-bottom: 1px solid #e0e0e0;
+  padding: 1em 1.5em;
+  overflow: auto;
+  word-wrap: break-word;
+  > * {
+    margin: 0.5em;
+  }
+}
+.attachment-card{
+  margin-bottom: 0.5em;
+}
+.comment{
+  padding: 1em;
+}
+.time{
+  padding: 1em;
+  text-align: right;
+  border-bottom: 1px solid #e0e0e0;
+}
+</style>
