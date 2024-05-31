47a0ab6374a0bf9b8973185fd6723f943701b8e8
CHEN Prime <chenbprime@outlook.com>
2024-05-31 10:11:09 +0800

    修复：修复作业详情不美观的问题

--- src/views/teacher/TaskCorrectView.vue
+++ src/views/teacher/TaskCorrectView.vue
@@ -2,57 +2,101 @@
   <div>
     <custom-float-back-button />
     <div
-      class="flex flex-col h-full"
+      class="flex flex-col h-full bsm:gap-8 sm:gap-2"
     >
-      <div class="correct-detail">
-        <div class="task-title">
-          {{ answer.taskName }}<p>({{ answer.courseName }}-{{ answer.sectionName }})</p>
-        </div>
-        <div class="correct-content">
+      <div class="flex flex-col border bg-white">
+        <span class="p-4 font-bold text-white bg-primary text-xl leading-4">
+          {{ answer.taskName }}<span>（{{ answer.courseName }}-{{ answer.sectionName }}）</span>
+        </span>
+        <div class="p-4 max-h-20 overflow-y-auto">
           {{ correct.taskContent }}
         </div>
       </div>
-      <div class="student">
-        <div class="student-answer">
-          <p>学生答案:</p>
-          <div class="answer-content">
-            {{ correct.answerContent }}
+      <div class="flex bsm:gap-8 sm:gap-2 flex-grow md:flex-col ">
+        <div class="flex-grow border bg-white md:w-full bmd:w-0 p-4 flex flex-col md:h-64">
+          <div class="flex flex-col gap-4 flex-grow h-0">
+            <p class="font-bold">
+              学生答案:
+            </p>
+            <div class="h-0 flex-grow overflow-y-auto break-words">
+              {{ correct.answerContent }}
+            </div>
           </div>
-          <div class="time">
+          <div class="text-end text-gray-500">
             {{ formatTime(answer.submitTime) }}
           </div>
         </div>
-        <div class="student-info-comment">
-          <div class="student-info">
-            <p>{{ answer.number }} - {{ answer.studentName }}</p>
-            <p>{{ answer.groupName }}</p>
+        <div class="flex-grow border bg-white md:w-full bmd:w-0 flex flex-col md:h-96 md:mb-8 sm:mb-2">
+          <div class="flex justify-between bg-primary text-white p-4 font-bold text-xl leading-4">
+            <span>{{ answer.number }} - {{ answer.studentName }}</span>
+            <span>{{ answer.groupName }}</span>
           </div>
-          <div class="comment">
-            <p><span>评语:</span> <span>分数 : <span :style="{ color: getScoreColor(correct.score) }">{{ correct.score }}</span></span></p>
-            <div v-if="correct.comment">
+          <div class="p-4 h-0 flex-grow font-bold flex flex-col overflow-y-auto">
+            <div class="flex justify-between mb-4">
+              <span>评语:</span>
+              <span>分数 : 
+                <span :style="{ color: getScoreColor(correct.score) }">{{ correct.score }}</span>
+              </span>
+            </div>
+            <div
+              v-if="correct.comment"
+              class="overflow-y-auto h-0 flex-grow break-words"
+            >
               {{ correct.comment }}
             </div>
-            <div v-else>
-              <h1>无评语</h1>
+            <div
+              v-else
+              class="flex justify-center items-center"
+            >
+              <v-icon
+                size="50px"
+                class="text-gray-400"
+                icon="mdi-text-box-remove-outline"
+              />
+              <p class="font-bold text-lg text-gray-400">
+                无评语
+              </p>
             </div>
           </div>
-          <div class="attachments">
-            <div>
-              <div v-if="correct.attachments">
-                <custom-attachment-card
-                  v-for="attachment in correct.attachments"
-                  :key="attachment.id"
-                  class="attachment-card"
-                  :attachment="attachment"
-                />
-              </div>
-              <div v-else>
-                <p>当前作业无附件</p>
-              </div>
+          <div class="p-4 border-t h-0 flex-grow overflow-y-auto">
+            <div
+              v-if="correct.attachments && correct.attachments.length > 0"
+              class="flex flex-col gap-4"
+            >
+              <custom-attachment-card
+                v-for="attachment in correct.attachments"
+                :key="attachment.id"
+                :attachment="attachment"
+              />
+            </div>
+            <div
+              v-else
+              class="flex justify-center items-center"
+            >
+              <v-icon
+                size="50px"
+                class="text-gray-400"
+                icon="mdi-attachment-off"
+              />
+              <p class="font-bold text-lg text-gray-400">
+                无附件
+              </p>
             </div>
           </div>
         </div>
       </div>
+      <v-overlay
+        v-model="loading"
+        class="align-center justify-center"
+        persistent
+        contained
+      >
+        <v-progress-circular
+          color="primary"
+          size="100"
+          indeterminate
+        />
+      </v-overlay>
     </div>
   </div>
 </template>
@@ -63,18 +107,14 @@ export default {
   name: 'TaskCorrectView',
   data() {
     return{
-    loading: true,
-    answer: {},
-    correct : {},
+      loading: true,
+      answer: {},
+      correct : {},
     };
   },
-  async created() {
+  created() {
     this.correctId = this.$route.params.id;
-    this.loading = true;
-    const res = await CorrectedAnswer(this.correctId);
-    this.correct = res.data;
-    this.answer = res.data.answer;
-    console.log(this.correct);
+    this.loadAnswer();
   },
   methods: {
     getScoreColor(score) {
@@ -86,107 +126,22 @@ export default {
         return 'red';
       }
     },
-     formatTime(timeString) {
+    formatTime(timeString) {
       const date = new Date(timeString);
       return date.toLocaleDateString('zh-CN', { year: 'numeric', month: '2-digit', day: '2-digit' });
     },
+    async loadAnswer() {
+      this.loading = true;
+      const res = await CorrectedAnswer(this.correctId);
+      if (res && res.data) {
+        this.correct = res.data;
+        this.answer = res.data.answer;
+      } else {
+        mitt.emit('showToast', { title: '获取题目答案信息失败', color: 'error', icon: '$error' });
+        this.$router.go(-1);
+      }
+      this.loading = false;
+    }
   },
 };
 </script>
-<style lang="scss" scoped>
-.correct-detail{
-  height: 24%;
-  border: 1px solid #e0e0e0;
-  .task-title{
-    height: 35%;
-    display: flex;
-    align-items: center;
-    font-weight: bold;
-    font-size: large;
-    background-color: #383838;
-    color:white;
-    padding: 0 1em;
-    p{
-      padding: 0 1em;
-      font-size: 16px;
-      font-weight: normal;
-    }
-  }
-  .correct-content{
-    height: 65%;
-    display: flex;
-    overflow: auto;
-    padding: 0.5em 1.5em;
-    background: white;
-  }
-}
-.student{
-  height: 70%;
-  display: flex;
-  margin-top: 2em;
-  .student-answer{
-    margin-right: 2em;
-    width: 50%;
-    border: 1px solid #e0e0e0;
-    background: white;
-    .answer-content{
-      height: 84%;
-      overflow: auto;
-      padding: 1em 1em 0 1em;
-    }
-    p{
-      display: flex;
-      align-items: center;
-      height: 8%;
-      padding: 0 1em;
-      font-weight: bold;
-      font-size: large;
-    }
-    .time{
-      height: 6%;
-      text-align: right;
-      padding-right: 1.25em;
-    }
-  }
-  .student-info-comment{
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    background: white;
-    border: 1px solid #e0e0e0;
-    .student-info{
-      height: 10%;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      border-bottom: 1px solid #e0e0e0;
-      background: #383838;
-      color:white;
-      padding: 0 1em;
-    }
-    .comment{
-      height: 45%;
-      overflow: auto;
-      padding: 1em;
-      border-bottom: 1px solid #e0e0e0;
-      p{
-        display: flex;
-        justify-content: space-between;
-        font-weight: bold;
-        margin: 0;
-      }
-      div{
-        padding: 1em 0;
-      }
-    }
-    .attachments{
-      height: 45%;
-      overflow: auto;
-      padding: 1em;
-      .attachment-card{
-        margin: 0.5em 0;
-      }
-    }
-  }
-}
-</style>
